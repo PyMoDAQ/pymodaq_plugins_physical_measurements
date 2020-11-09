ea413dbea58fd69c074922465d81cc5444a77398
Sébastien Weber <seba.weber@gmail.com>
2020-11-09 17:08:20 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,16 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'physical_measurements'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_physical_measurements' #to modify
+description = 'Set of PyMoDAQ plugins for various physical measurements: multimeter, lockin, oscilloscope, indus cameras...'
+
+
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = ['pyvisa',
+                     'bitstring',
+                     'harvesters', #for genicam compliant cameras
+                     'git+https://github.com/morefigs/py-ic-imaging-control'] #for the imaging source cameras (should be updated using manufacturer set of python functions
+##
